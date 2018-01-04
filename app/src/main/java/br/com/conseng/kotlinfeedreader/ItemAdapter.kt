41a2b4b42bc035b3c02342470818e2af167c4fdb
Fernando Camargo <fernandoantonio.camargo@gmail.com>
2018-01-04 15:04:38 -0200

    Codigo em elaboração. Criado o adapter.

---
+++ app/src/main/java/br/com/conseng/kotlinfeedreader/ItemAdapter.kt
@@ -0,0 +1,70 @@
+package br.com.conseng.kotlinfeedreader
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by Qin on 04/01/2018.
+ */
+class ItemAdapter(val list: ArrayList<MainActivity.Item>, val context: Context) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    /**
+     * Preserva os dados de um artigo para agilizar no "scroll", enquanto reciclas as views.
+     * @param [view] Artigo que será preservado durante a reciclagem.
+     */
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titulo = view.findViewById(R.id.post_titulo) as TextView
+        val autor = view.findViewById(R.id.post_autor) as TextView
+        val data = view.findViewById(R.id.post_data) as TextView
+        val image = view.findViewById(R.id.post_imagem) as ImageView
+        val bthVerMais = view.findViewById(R.id.post_ver_detalhes) as Button
+    }
+
+    /**
+     * Called when RecyclerView needs a new [ItemViewHolder] of the given type to represent
+     * an item.   Create a new View inflating it from an XML layout file.
+     * The new ViewHolder will be used to display items of the adapter using
+     * [onBindViewHolder].
+     * @param [parent] The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param [viewType] The view type of the new View.
+     * @return The new ViewHolder that holds the View of the given view type.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ItemViewHolder {
+        val vh = LayoutInflater.from(parent?.context).inflate(R.layout.item_list, parent, false)
+        val ivh = ItemViewHolder(vh)
+        return ivh
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the [ItemViewHolder] to reflect the item at the given position.
+     * @param [holder] The ViewHolder which should be updated to represent the contents of the
+     *        item at the given position in the data set.
+     * @param [position] The position of the item within the adapter's data set.
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder?, position: Int) {
+        holder?.titulo?.text = list[position].titulo
+        holder?.autor?.text = list[position].autor
+        holder?.data?.text = SimpleDateFormat("dd/MMM/yyyy", Locale("pt", "BR")).format(Date(list[position].data))
+//        holder?.image?.i = list[position].titulo
+        holder?.bthVerMais?.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, list[position].link)
+            context.startActivity(intent)
+        }
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.     *
+     * @return The total number of items in this adapter.
+     */
+    override fun getItemCount(): Int = list.size
+}
--- app/src/main/java/br/com/conseng/kotlinfeedreader/MainActivity.kt
+++ app/src/main/java/br/com/conseng/kotlinfeedreader/MainActivity.kt
@@ -1,12 +1,48 @@
 package br.com.conseng.kotlinfeedreader
 
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.pkmmte.pkrss.Article
+import com.pkmmte.pkrss.Callback
+import com.pkmmte.pkrss.PkRSS
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Callback {
+
+    val listItens = arrayListOf<Item>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        PkRSS.with(this).load("https://rss.tecmundo.com.br/feed").callback(this).async()
     }
+
+    override fun onLoadFailed() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPreload() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    /**
+     * Assim que termina de carregar o feed, retorna uma lista de artigos.
+     * @param [newArticles] Lista de artigos recebida
+     */
+    override fun onLoaded(newArticles: MutableList<Article>?) {
+        newArticles?.mapTo(listItens) {
+            Item(it.title, it.author, it.date, it.source, it.enclosure.url)
+        }
+    }
+
+    /**
+     * Salva as informações básicas de um artigo.
+     * @param [titulo] Título do artigo.
+     * @param [autor] Nome do autor.
+     * @param [data] Data que a RSS foi postada.
+     * @param [link] URI onde informações adicionais estão disponíveis.
+     * @param [imagem] URL da foto ilustrativa.
+     */
+    data class Item(val titulo: String, val autor: String, val data: Long, val link: Uri, val imagem: String)
 }
